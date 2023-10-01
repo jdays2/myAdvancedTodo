2a98c34da021ce0bbfb695104ab35f544bbe1c8c
Марк <lolka-pop@rambler.ru>
2023-10-02 01:01:48 +0300

    feat: add edit for todo

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,96 +1,257 @@
 import React, { useState } from 'react';
-import { Card, Space, Select, Dropdown, Drawer } from 'antd';
-import { MoreOutlined } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import {
+	Card,
+	Space,
+	Form,
+	Select,
+	Dropdown,
+	Drawer,
+	Row,
+	Col,
+	Input,
+	DatePicker,
+	Button,
+} from 'antd';
 
-const items = [
+import dayjs from 'dayjs';
+
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import { MoreOutlined } from '@ant-design/icons';
+import TextArea from 'antd/es/input/TextArea';
+
+const dateFormat = 'YYYY-MM-DD';
+dayjs.extend(customParseFormat);
+
+const selectItems = [
 	{
-		key: '1',
-		label: <button>Edit</button>,
+		value: 'active',
+		label: 'active',
 	},
 	{
-		key: '2',
-		danger: true,
-		label: <button>Remove</button>,
+		value: 'resolved',
+		label: 'resolved',
 	},
 ];
 
 export default function TodoCard() {
 	const [open, setOpen] = useState(false);
+	const [active, setActive] = useState(true);
+	const [deleted, setDeleted] = useState(false);
+	const [openCard, setOpenCard] = useState(false);
 
-	const showDrawer = (e) => {
-		if (!e.target.classList.contains('card__link')) {
-			setOpen(true);
+	const openCardHandler = (e) => {
+		if (
+			deleted ||
+			e.target.closest('.card__select') ||
+			e.target.closest('.ant-dropdown-trigger') ||
+			e.target.closest('.ant-dropdown-menu-title-content') ||
+			e.target.closest('.ant-select-item-option-content')
+		) {
+			return;
 		}
+		setOpenCard(true);
 	};
+
+	const closeCardHandler = () => {
+		setOpenCard(false);
+	};
+
+	const showEdit = () => {
+		setOpen(true);
+	};
+
 	const onClose = () => {
 		setOpen(false);
 	};
 
+	const selectHandler = (value) => {
+		if (value === 'active') {
+			setActive(true);
+		} else {
+			setActive(false);
+		}
+	};
+
+	const deleteCard = () => {
+		setDeleted(true);
+	};
+
+	const selectActiveClass = active
+		? 'card__select--active'
+		: 'card__select--resolved';
+
+	const cardDeleted = deleted ? 'card--deleted' : '';
+
+	const items = [
+		{
+			key: '1',
+			label: <button onClick={showEdit}>Edit</button>,
+		},
+		{
+			key: '2',
+			danger: true,
+			label: <button onClick={deleteCard}>Remove</button>,
+		},
+	];
+
 	return (
 		<>
 			<Card
-				onClick={showDrawer}
-				headStyle={{ borderBottom: 'none' }}
-				bodyStyle={{ padding: 0, cursor: 'pointer' }}
-				hoverable
-				style={{
-					width: '40rem',
-					background: '#fff',
-					border: '0.1rem solid #0000001a',
-					borderRadius: '0.8rem',
-					boxShadow: '0 0.2rem 0.8rem #0000000a, 0 0.4rem 2.4rem #0000000a',
-					color: '#262525',
-					cursor: 'default',
-					padding: '1.2rem',
-				}}>
-				<div className="card__content">
-					<p className="card__date">01.10.23 03:24</p>
+				hoverable={deleted ? false : true}
+				className={`card ${cardDeleted}`}>
+				<div
+					className="card__content"
+					onClick={openCardHandler}>
+					<div className="card__head">
+						<p className="card__date">01.10.23 03:24</p>
+						{deleted ? null : (
+							<Dropdown
+								menu={{ items }}
+								placement="bottom">
+								<p className="card__link">
+									<MoreOutlined />
+								</p>
+							</Dropdown>
+						)}
+					</div>
 					<p className="card__title">Hello</p>
-					<Select
-						defaultValue="active"
-						style={{
-							width: '12rem',
-							height: '3rem',
-							padding: '0.1rem',
-							// backgroundColor: "#ffdb4d4d",
-							backgroundColor: '#3bc9351a',
-							color: '#07a300',
-							borderRadius: '1rem',
-						}}
-						bordered={false}
-						options={[
-							{
-								value: 'active',
-								label: 'active',
-							},
-							{
-								value: 'resolved',
-								label: 'resolved',
-							},
-						]}
-					/>
-
-					<Dropdown
-						menu={{
-							items,
-						}}
-						placement="bottom">
-						<p className="card__link">
-							<MoreOutlined />
-						</p>
-					</Dropdown>
+					{deleted ? (
+						<span className="card__status">DELETED</span>
+					) : (
+						<Select
+							defaultValue="active"
+							className={`card__select ${selectActiveClass}`}
+							bordered={false}
+							onChange={selectHandler}
+							options={selectItems}
+						/>
+					)}
 				</div>
 			</Card>
-			{/* <Drawer
-				title="Edit"
+			<Drawer
+				title="Edit TODO CARD"
 				placement="right"
 				onClose={onClose}
-				open={open}>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-			</Drawer> */}
+				open={open}
+				extra={
+					<Space>
+						<Button onClick={onClose}>Cancel</Button>
+						<Button
+							onClick={onClose}
+							type="primary">
+							Submit
+						</Button>
+					</Space>
+				}>
+				<Form
+					layout="vertical"
+					hideRequiredMark>
+					<Row gutter={16}>
+						<Col span={12}>
+							<Form.Item
+								name="name"
+								label="Title"
+								rules={[
+									{
+										required: true,
+										message: 'Required field',
+									},
+								]}>
+								<Input defaultValue="Your Title" />
+							</Form.Item>
+						</Col>
+						<Col span={12}>
+							<Form.Item
+								name="priority"
+								label="Priority"
+								rules={[
+									{
+										required: true,
+										message: 'Required field',
+									},
+								]}>
+								<Select
+									placeholder="Please choose the type"
+									defaultValue={'standard'}>
+									<Option value="critical">Critical</Option>
+									<Option value="urgent">Urgent</Option>
+									<Option value="standard">Standard</Option>
+								</Select>
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={12}>
+							<Form.Item
+								name="type"
+								label="Type"
+								rules={[
+									{
+										required: true,
+										message: 'Please choose the type',
+									},
+								]}>
+								<Select
+									placeholder="Please choose the type"
+									defaultValue={'Work'}>
+									<Option value="Personal">Personal</Option>
+									<Option value="Work">Work</Option>
+								</Select>
+							</Form.Item>
+						</Col>
+
+						<Col span={12}>
+							<Form.Item
+								name="deadline"
+								label="Deadline"
+								rules={[
+									{
+										required: true,
+										message: 'Please choose the date',
+									},
+								]}>
+								<DatePicker
+									defaultValue={dayjs("2022.02.10", dateFormat)}
+									style={{
+										width: '100%',
+									}}
+								/>
+							</Form.Item>
+						</Col>
+					</Row>
+					<Row gutter={16}>
+						<Col span={24}>
+							<Form.Item
+								name="body"
+								label="body"
+								rules={[
+									{
+										required: true,
+										message: 'Required field',
+									},
+								]}>
+								<TextArea
+									rows={6}
+									defaultValue="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo,
+									sapiente. Excepturi nam voluptatum omnis voluptates, eum odio
+									quam in accusamus sunt totam maiores incidunt. Laudantium quasi
+									odit ea hic accusamus."
+									placeholder="maxLength is 250"
+									maxLength={250}
+								/>
+							</Form.Item>
+						</Col>
+					</Row>
+				</Form>
+			</Drawer>
+
+			<Drawer
+				title="TODO CARD"
+				placement="right"
+				onClose={closeCardHandler}
+				open={openCard}>
+				123
+			</Drawer>
 		</>
 	);
 }
